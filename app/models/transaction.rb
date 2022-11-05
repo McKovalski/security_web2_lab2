b96b7417ec142e5e58baaba6a60421a20b6d1f23
ninokovacic <nkovai888@gmail.com>
2022-11-05 18:36:40 +0100

    Typo fix

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,4 @@
 class Transaction < ApplicationRecord
     belongs_to :from_user, foreign_key: :from_user_id, class_name: 'User'
-    belongs_to :to_user, foreign_key: :to_user_id, class_name: 'Team'
+    belongs_to :to_user, foreign_key: :to_user_id, class_name: 'User'
 end
