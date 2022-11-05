4e24b7b01de54c25c2db20d31e396beb7fd42935
ninokovacic <nkovai888@gmail.com>
2022-11-05 15:56:21 +0100

    Devise

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create!([
+    {
+        :email                 => "ivica.ivic@gmail.com",
+        :password              => "password",
+        :password_confirmation => "password"
+    },
+    {
+        :email                 => "marica.maric@gmail.com",
+        :password              => "password",
+        :password_confirmation => "password"
+    },
+    {
+        :email                 => "jurica.juric@gmail.com",
+        :password              => "password",
+        :password_confirmation => "password"
+    },
+    {
+        :email                 => "vjekoslav.vjekic@gmail.com",
+        :password              => "password",
+        :password_confirmation => "password"
+    }
+])
